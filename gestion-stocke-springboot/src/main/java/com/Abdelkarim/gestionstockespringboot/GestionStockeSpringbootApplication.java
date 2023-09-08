86fe366c38f3d7281615890e0bdd33ab742d7795
ABDELKARIM2021 <elouaddany.k99@gmail.com>
2023-09-08 19:46:35 +0100

    first cmmit

---
+++ gestion-stocke-springboot/src/main/java/com/Abdelkarim/gestionstockespringboot/GestionStockeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.Abdelkarim.gestionstockespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockeSpringbootApplication.class, args);
+	}
+
+}
---
+++ gestion-stocke-springboot/src/test/java/com/Abdelkarim/gestionstockespringboot/GestionStockeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Abdelkarim.gestionstockespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
